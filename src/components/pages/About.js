4dc4ce50b3f7cd759f09449b117287f0f7a3cec8
John Druhan <jfrancisdruhan@comcast.net>
2019-05-09 14:21:00 -0400

    setup

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple App To Manage Your Contacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+};
